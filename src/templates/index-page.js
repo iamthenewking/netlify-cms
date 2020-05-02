1f98517c289540d593d7b12522c8ff6db0ab1869
jeffrey gaboni <mrgabonijeff@gmail.com>
2020-05-02 08:56:04 +0800

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -2,8 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
+//import Layout from '../components/Layout'
+//import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
